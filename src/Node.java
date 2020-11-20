297e7e83e60b456ffd70b2ec90f101ff31644dc2
Meshal khalid <33171761+MeshalKhalid@users.noreply.github.com>
2020-11-20 13:22:59 +0300

    add: new prop heuristicValue , used with informed search algorithms add: new prop stepCost , used to indicate the value of each step

--- src/Node.java
+++ src/Node.java
@@ -1,43 +1,48 @@
 
 public class Node {
 
-	private int[] state;
-	private Node parent;
-	private double pathCost;
+    private int[] state;
+    private Node parent;
+    private double heuristicValue;
+    private double stepValue = 1;
 
-	public Node(int[] state, double pathCost) {
-		super();
-		this.state = state;
-		this.pathCost = pathCost;
-		this.parent = null;
-	}
+    public Node(int[] state, double heuristicValue) {
+        super();
+        this.state = state;
+        this.heuristicValue = heuristicValue;
+        this.parent = null;
+    }
 
-	public Node(int[] state, double pathCost, Node parent) {
-		super();
-		this.state = state;
-		this.pathCost = pathCost;
-		this.parent = parent;
-	}
+    public Node(int[] state, double heuristicValue, Node parent) {
+        super();
+        this.state = state;
+        this.heuristicValue = heuristicValue;
+        this.parent = parent;
+    }
 
 
-	public int[] getState() {
-		return state;
-	}
+    public int[] getState() {
+        return state;
+    }
 
-	public Node getParent() {
-		return parent;
-	}
+    public double getStepValue() {
+        return stepValue;
+    }
 
-	public void setState(int[] state) {
-		this.state = state;
-	}
+    public Node getParent() {
+        return parent;
+    }
 
-	public double getPathCost() {
-		return pathCost;
-	}
+    public void setState(int[] state) {
+        this.state = state;
+    }
 
-	public void setPathCost(int pathCost) {
-		this.pathCost = pathCost;
-	}
+    public double getHeuristicValue() {
+        return heuristicValue;
+    }
+
+    public void setPathCost(int pathCost) {
+        this.heuristicValue = pathCost;
+    }
 
 }
